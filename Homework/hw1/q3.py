6a30fe336c431746e9345dc9678bb0273226b5a9
JiaqiZhao2004 <royzhao2004@outlook.com>
2025-10-15 17:43:36 -0600

    q3 try 1

--- Homework/hw1/q3.py
+++ Homework/hw1/q3.py
@@ -4,22 +4,50 @@
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 import torch
 import torch.distributed as dist
+import torch.nn.functional as F
 
 def reduce_scatter(chunks, tmp, world, rank, left, right):
-    #                                                                   #
-    #                                                                   #
     # your code here: follow slides instruction: do counter-clockwise iteration
-    #                                                                   #
-    #                                                                   #
-    return
+
+    send_idx = rank
+    for r in range(world - 1):
+        recv_idx = (send_idx - 1) % world
+
+        # send self and receive from left
+        s_req = dist.isend(chunks[send_idx], dst=right)
+        r_req = dist.irecv(tmp, src=left)
+
+        # update self
+        r_req.wait()
+        chunks[recv_idx].add_(tmp)
+
+        # wait for send to finish
+        s_req.wait()
+
+        # rotate to send the just-updated chunk
+        send_idx = recv_idx
         
 def all_gather(chunks, tmp, current, world, rank, left, right):
-    #                                                                   #
-    #                                                                   #
     # your code here: follow slides instruction: do counter-clockwise iteration
-    #                                                                   #
-    #                                                                   #
-    return
+
+    send_idx = (rank + 1) % world
+    for r in range(world - 1):
+        recv_idx = (send_idx - 1) % world
+
+        # send self and receive from left
+        s_req = dist.isend(chunks[send_idx], dst=right)
+        r_req = dist.irecv(tmp, src=left)
+
+        # update self
+        r_req.wait()
+        chunks[recv_idx].copy_(tmp)
+
+        # wait for send to finish
+        s_req.wait()
+
+        # rotate to send the just-updated chunk
+        send_idx = recv_idx
+
 
 def ring_allreduce_(tensor: torch.Tensor, world_size = None, rankid = None):
     """In-place ring all-reduce (SUM, optional average) using isend/irecv."""
@@ -32,27 +60,23 @@ def ring_allreduce_(tensor: torch.Tensor, world_size = None, rankid = None):
     flat = tensor.contiguous().view(-1)
     n = flat.numel()
     chunk = (n + world - 1) // world
-    #                                                                   #
-    #                                                                   #
     # your code here: we cannot divide flat into 3 pieces evenly as the
     # flat lengh may not be able to divided exactly by 3....
-    #
-    #                                                                   #
-    #                                                                   #
     #So, fill zeros at the end of flat to generate padded_flat
-    padded_flat = None # modify this line and fill correct value into padded_flat
+    padded_flat = F.pad(flat, (0, chunk * world - n), mode='constant', value=0)
+
     chunks = [padded_flat[i*chunk:(i+1)*chunk] for i in range(world)]
 
-    #                                                                   #
-    #                                                                   #
     # your code here: call reduce_scatter and all_gather
-    #
-    #                                                                   #
-    #                                                                   #
-    #we provide the reduce_scatter and all_gather func prototype for you
+    tmp = torch.empty_like(chunks[0])
+    reduce_scatter(chunks, tmp, world, rank, left, right)
+    all_gather(chunks, tmp, world, rank, left, right)
+
+    # we provide the reduce_scatter and all_gather func prototype for you
     # You may adjust the function signature (input structure) of `reduce_scatter` and `all_gather` if needed.
     
     # stitch & unpad  
+    flat = torch.cat(chunks, dim=0)[:n]
     flat /= world
-    tensor.view(-1).copy_(flat[:n])
-    return
+    tensor.view(-1).copy_(flat)
+    return tensor
